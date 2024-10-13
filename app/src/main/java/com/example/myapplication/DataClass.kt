2bf671cb9862c0ac182b028cf7efd35b8f83fdbc
safirasn21 <safirasn021@gmai.com>
2024-10-14 01:21:19 +0700

    membuat data class dengan id dan email

---
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+//data class adalah class yang digunakan untuk menyimpan data dan berfungsi equals, hashcode, dan to string
+// data class menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+fun main(){
+    val data = DataClass(1, "safira@gmail.com")
+    println(data)
+
+}
