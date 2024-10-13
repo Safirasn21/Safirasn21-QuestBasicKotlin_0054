2b50c4eba3822973d2afc31933fd5582e5d8d815
safirasn21 <safirasn021@gmai.com>
2024-10-13 22:30:59 +0700

    - menampilkan email

--- app/src/main/java/com/example/myapplication/Class.kt
+++ app/src/main/java/com/example/myapplication/Class.kt
@@ -4,3 +4,9 @@ class Motor()
 
 class Contact(val id: Int, var email: String)
 
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    //menampilkan value email
+    println(contact.email)
+}
